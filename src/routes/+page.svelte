93f44fe09221947973a43dbf350cef10ac89a8a1
PuwaritK <puwarit420@gmail.com>
2024-11-02 19:11:39 +0700

    fix: change README

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
-<div class="relative flex flex-col items-center">
-	<h1>Welcome to SELS</h1>
-	<p>a fantasy game that will feed your gambling addiction.</p>
-	<p>Login or signup now.</p>
+<div class="relative flex flex-col items-center space-y-8">
+	<div class="font-serif text-8xl font-bold">
+		<h1>Welcome to SELS</h1>
+	</div>
+	<div class="space-y-8 text-center align-middle font-serif text-4xl">
+		<p>A fantasy game that will feed your gambling addiction.</p>
+		<p>Login or signup now.</p>
+	</div>
 </div>
