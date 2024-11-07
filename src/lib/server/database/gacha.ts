14022db1561076d94563bffc1863a819029578c7
PuwaritK <puwarit420@gmail.com>
2024-11-07 23:37:33 +0700

    feat: create skeleton for gacha

--- src/lib/server/database/gacha.ts
+++ src/lib/server/database/gacha.ts
@@ -1,6 +1,21 @@
 import { getAccount, getCurrency } from './account';
-import type { account } from '@prisma/client';
-export const randomSel = async (account: account, pullAmount: number = 1) => {
+import type { account, paradise } from '@prisma/client';
+import prisma from './client';
+
+export const randomSel = async (account: account, paradise: paradise) => {
+	// let generatedSel = await prisma.sel.create({
+	// 	data: {
+	// 		dob: new Date(Date.now()),
+	// 		colour: 'grey',
+	// 		weight: 69,
+	// 		price: 6969,
+	// 		type:,
+	// 		sex:,
+	// 	}
+	// });
+};
+
+export const pullSel = async (account: account, pullAmount: number = 1) => {
 	const GACHACOST = 300;
 	let currency = (await getCurrency(account.user_id)) as number;
 	switch (pullAmount) {
