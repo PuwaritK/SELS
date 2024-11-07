8b7092c83cbcb2e012c17eb3899043f15392ab51
PuwaritK <puwarit420@gmail.com>
2024-11-08 00:44:41 +0700

    fix: remove console.log

--- src/routes/sels/+layout.server.ts
+++ src/routes/sels/+layout.server.ts
@@ -3,7 +3,6 @@ import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async (event) => {
-	console.log(event.locals.user);
 	if (event.locals.user === null) {
 		return redirect(302, '/signin?from=' + event.url.pathname.substring(1));
 	}
