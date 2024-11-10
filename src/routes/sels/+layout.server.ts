fd2769e1f2a38d2e1269a0de298ade672e698dd9
PuwaritK <puwarit420@gmail.com>
2024-11-11 04:46:18 +0700

    fix: removed obfuscated export

--- src/routes/sels/+layout.server.ts
+++ src/routes/sels/+layout.server.ts
@@ -6,5 +6,4 @@ export const load: LayoutServerLoad = async (event) => {
 	if (event.locals.user === null) {
 		return redirect(302, '/signin?from=' + event.url.pathname.substring(1));
 	}
-	// ...
 };
