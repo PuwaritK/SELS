67d48bb226d7343a85af1aa515a3ccbaf6629b53
PuwaritK <puwarit420@gmail.com>
2024-11-11 16:58:19 +0700

    feat: add redirect to newbie

--- src/routes/sels/+layout.server.ts
+++ src/routes/sels/+layout.server.ts
@@ -5,5 +5,7 @@ import type { LayoutServerLoad } from './$types';
 export const load: LayoutServerLoad = async (event) => {
 	if (event.locals.user === null) {
 		return redirect(302, '/signin?from=' + event.url.pathname.substring(1));
+	} else if (event.locals.user.paradise_id === null && event.url.pathname != '/sels/newbie') {
+		return redirect(302, '/sels/newbie');
 	}
 };
