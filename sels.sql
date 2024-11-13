e75da03ab771ddd5bcca081d11533671b6357c5d
PuwaritK <puwarit420@gmail.com>
2024-11-14 01:19:53 +0700

    fix: change date to datetime in sel table

--- sels.sql
+++ sels.sql
@@ -1,6 +1,5 @@
 SET FOREIGN_KEY_CHECKS = 0;
-
-/*!999999\- enable the sandbox mode */ 
+/*M!999999\- enable the sandbox mode */ 
 INSERT INTO `account` VALUES
 (1,1,'sel','$argon2id$v=19$m=65536,t=3,p=4$HGoK6olcn1qSmEpTaNSwUQ$1CQInO9V42DcGvan36hODZZ90QRu7E7EuR58kp1t6tY',1,0);
 INSERT INTO `paradise` VALUES
@@ -10,7 +9,7 @@ INSERT INTO `role` VALUES
 (2,'admin','/admin'),
 (3,'developer','/developer');
 INSERT INTO `sel` VALUES
-(1,'Firm',1,'#ffffff',1,'2024-11-12',8,0,0,1,1);
+(1,'Firm',1,'#ffffff',1,'2024-11-12 00:00:00',8,0,0,1,1);
 INSERT INTO `session` VALUES
 ('b7cf013c7ee2784899eb01346329b40b36d2b3f1e2dedcd43398a0fe63eb4ce1',1,'2024-11-19 15:06:38.325');
 INSERT INTO `sex` VALUES
@@ -31,6 +30,4 @@ INSERT INTO `type` VALUES
 (5,'Leopard seal'),
 (6,'Sea lion'),
 (7,'Walrus');
-
 SET FOREIGN_KEY_CHECKS = 1;
-
