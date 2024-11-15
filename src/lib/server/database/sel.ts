946330af4a6ef4fb59401a6e663a3a48583cbe80
fodnia <fodnia-san@proton.me>
2024-11-15 20:02:16 +0700

    feat: add C/D sel function

--- src/lib/server/database/sel.ts
+++ src/lib/server/database/sel.ts
@@ -6,35 +6,65 @@ export const createSel = async (
 	sel_type: number,
 	sel_colour: string,
 	sel_sex: number,
-	sel_weight: number = 8,
-	sel_tier: number
+	sel_tier: number,
+	sel_paradise: number,
+	sel_weight: number,
+	sel_xp: number,
+	sel_price: number
 ) => {
-	//TODO: for patton
+	const createSel = await prisma.sel.create({
+		data: {
+			name: sel_name,
+			type_id: sel_type,
+			colour: sel_colour,
+			dob: new Date(Date.now()),
+			weight: sel_weight,
+			show_xp: sel_xp,
+			price: sel_price,
+			tier_id: sel_tier,
+			sex_id: sel_sex,
+			paradise_id: sel_paradise
+		}
+	})
 };
 
-export const updateSel = async (
-	sel_id: number,
-	sel_name: string,
-	sel_type: number,
-	sel_colour: string,
-	sel_sex: number
-	//add or remove more params as needed
-) => {
-	//TODO: for patton
-};
+// export const updateSel = async (
+// 	sel_id: number,
+// 	sel_name: string,
+// 	sel_type: number,
+// 	sel_colour: string,
+// 	sel_weight: number,
+// 	sel_tier: number
+// ) => {
+// 	const upgradeSel = await prisma.sel.update({
+// 		where: {
+// 			sel_id: sel_id
+// 		},
+// 		data: {
+// 			name: sel_name,
+// 			type_id: sel_type,
+// 			colour: sel_colour,
+// 			weight: sel_weight,
+// 			tier_id: sel_tier
+// 		}
+// 	})
+// };
 
 export const deleteSel = async (
 	sel_id: number
-	//add or remove more params as needed
 ) => {
-	//TODO: for patton
+	const killSel = await prisma.sel.delete({
+		where: {
+			sel_id: sel_id
+		}
+	})
 };
 
 export const searchSel = async (searchOptions: SearchOptions, username: string) => {
 	//TODO: for patton
 };
 
-export const getAllSel = async (username: string) => {
+export const getAllSel = async (userID: number) => {
 	//TODO: for patton
 };
 
