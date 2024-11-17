fb8912722312f8dcede7fc95aa62b0dc43552338
PuwaritK <puwarit420@gmail.com>
2024-11-18 04:41:06 +0700

    feat: made buttons for manage page

---
+++ src/routes/sels/profile/manage/+page.server.ts
@@ -0,0 +1,8 @@
+import type { PageServerLoad, Actions } from '../$types';
+export const load: PageServerLoad = async (locals) => {};
+
+export const actions: Actions = {
+	feed: async ({ request }) => {},
+	show: async ({ request }) => {},
+	play: async ({ request }) => {}
+};
---
+++ src/routes/sels/profile/manage/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { enhance } from '$app/forms';
+</script>
+
+<div class="flex min-h-60 flex-row justify-center align-middle">
+	<form action="?/play" method="post" use:enhance>
+		<button type="submit" class="size-20">Play with Sels</button>
+	</form>
+	<form action="?/show" method="post" use:enhance>
+		<button type="submit" class="size-20">Host Sels show</button>
+	</form>
+	<form action="?/feed" method="post" use:enhance>
+		<button type="submit" class="size-20">Feed Sels</button>
+	</form>
+</div>
