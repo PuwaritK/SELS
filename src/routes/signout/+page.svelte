4828f437c537bfae976654ca3ce471fd1dd2b073
PuwaritK <puwarit420@gmail.com>
2026-05-29 23:47:56 +0700

    style: made texts bigger in signout page

--- src/routes/signout/+page.svelte
+++ src/routes/signout/+page.svelte
@@ -1,7 +1,6 @@
 <div class="flex h-dvh">
-	<div class="m-auto flex flex-col justify-center align-middle">
-		<p class="text-xl">You've signed out!</p>
-
-		<a href="/" class="text-blue-400">return to main page</a>
+	<div class="m-auto flex flex-col items-center justify-center align-middle">
+		<p class="text-4xl">You've signed out!</p>
+		<a href="/" class="text-4xl text-blue-400">Return to main page</a>
 	</div>
 </div>
